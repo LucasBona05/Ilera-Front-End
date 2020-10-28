099485f8de4bb7c53323f8c4d91df754913fd040
LucasBona05 <lucas.goncalves@ucsal.edu.br>
2020-10-28 20:42:21 -0300

    arquivos da tela de cadastro criados e cadastro criado no aluno repository

--- lib/app/repositories/aluno_repository.dart
+++ lib/app/repositories/aluno_repository.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:ilera/app/models/imagem_model.dart';
 import 'package:ilera/app/models/json_web_token_model.dart';
 import 'package:ilera/app/models/pessoa_model.dart';
 import 'package:ilera/app/repositories/token_repository.dart';
@@ -35,6 +36,68 @@ class AlunoRepository {
         peso: response.data['peso'],
         altura: response.data['altura'],
       );
+      // ignore: control_flow_in_finally
+      return aluno;
+    }
+  }
+
+  Future<AlunoModel> cadastro(
+      String nomeCompleto,
+      String senha,
+      String email,
+      String genero,
+      String cpf,
+      String telefone,
+      ImagemModel fotoDePerfil,
+      ImagemModel fotoDeFrente,
+      ImagemModel fotoDeCostas,
+      double peso,
+      double altura,
+      double cintura,
+      double pescoco,
+      String plano) async {
+    final Map<String, dynamic> bodyRequest = {
+      "nomeCompleto": nomeCompleto,
+      "senha": senha,
+      "email": email,
+      "genero": genero,
+      "cpf": cpf,
+      "telefone": telefone,
+      "fotoDePerfil": fotoDePerfil,
+      "fotoDeFrente": fotoDeFrente,
+      "fotoDeCostas": fotoDeCostas,
+      "peso": peso,
+      "altura": altura,
+      "cintura": cintura,
+      "pescoco": pescoco,
+      "plano": plano
+    };
+    JsonWebTokenModel jwt = await _token.getToken();
+    final Map<String, dynamic> headersRequest = {
+      "Authorization": "Bearer ${jwt.jwt}",
+    };
+    var response;
+    try {
+      response = await _dio.post('/usuarios/alunos/registrar',
+          data: bodyRequest, options: Options(headers: headersRequest));
+    } catch (e) {
+      print('Uma exception foi encontrada...');
+    } finally {
+      print(response.data);
+      AlunoModel aluno = new AlunoModel(
+          nomeCompleto: response.data['nomeCompleto'],
+          email: response.data['email'],
+          genero: response.data['genero'],
+          cpf: response.data['cpf'],
+          telefone: response.data['telefone'],
+          fotoDePerfil: response.data['fotoDePerfil'],
+          fotoDeFrente: response.data['fotoDeFrente'],
+          fotoDeCostas: response.data['fotoDeCostas'],
+          peso: response.data['peso'],
+          altura: response.data['peso'],
+          cintura: response.data['cintura'],
+          pescoco: response.data['pescoco'],
+          plano: response.data['plano']);
       // ignore: control_flow_in_finally
       return aluno;
     }
